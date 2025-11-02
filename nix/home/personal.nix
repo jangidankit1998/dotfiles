f0c7146b0160099861f6c959bb4317f32b603cd3
Ankit Jangid <jangidankit1998@gmail.com>
2025-11-02 17:04:35 +0530

    chore: removing cdk

--- nix/home/personal.nix
+++ nix/home/personal.nix
@@ -1,8 +1,9 @@
-{pkgs, ...}: let
+{pkgs, lib, ...}: let
   common = pkgs.callPackage ./packages.nix {inherit pkgs;};
 in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
+  services.kdeconnect.enable = lib.mkForce false;
   home.username = "ankitjangid";
   home.enableNixpkgsReleaseCheck = false;
   home.homeDirectory = "/Users/ankitjangid";
